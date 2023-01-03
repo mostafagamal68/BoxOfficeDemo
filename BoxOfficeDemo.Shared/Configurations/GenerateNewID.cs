f0328368b3af0e71e744cf9ded9dfde7917cfbfc
Mostafa Gamal <mostafa.gamal_98@hotmail.com>
2023-01-03 22:52:46 +0200

    some useless code removed

--- BoxOfficeDemo.Shared/Configurations/GenerateNewID.cs
+++ BoxOfficeDemo.Shared/Configurations/GenerateNewID.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BoxOfficeDemo.Shared.Configurations
+﻿namespace BoxOfficeDemo.Shared.Configurations
 {
     public class GenerateNewID
     {
