c1484c446e085ec3e0683a26b8c118171da5d140
MostafaGamal <mostafa.gamal@codzone-eg.com>
2023-01-04 17:25:20 +0200

    Toast Noftification added and removed messagealert component

---
+++ BoxOfficeDemo.Client/Services/Toast/ToastService.cs
@@ -0,0 +1,25 @@
+﻿using BoxOfficeDemo.Client.Configurations.Toast;
+
+namespace BoxOfficeDemo.Client.Services.Toast
+{
+    public class ToastService : IToastService
+    {
+        public event Action<ToastLevel, string, string> OnShow;
+
+        public void ShowError(string message, string heading = "") =>
+            ShowToast(ToastLevel.Error, message, heading);
+
+        public void ShowInfo(string message, string heading = "") =>
+            ShowToast(ToastLevel.Information, message, heading);
+
+        public void ShowSuccess(string message, string heading = "") =>
+            ShowToast(ToastLevel.Success, message, heading);
+
+        public void ShowWarning(string message, string heading = "") =>
+            ShowToast(ToastLevel.Warning, message, heading);
+
+        public void ShowToast(ToastLevel level, string message, string heading = "") =>
+            OnShow?.Invoke(level, message, heading);
+
+    }
+}
