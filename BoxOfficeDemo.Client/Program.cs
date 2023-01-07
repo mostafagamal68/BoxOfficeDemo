2dfa0c0fbdadbeeb58ce9fff8e8e937378250baf
Mostafa Gamal <mostafa.gamal_98@hotmail.com>
2023-01-07 14:58:11 +0200

    Modal Added for watchlist, login and registration

--- BoxOfficeDemo.Client/Program.cs
+++ BoxOfficeDemo.Client/Program.cs
@@ -1,6 +1,6 @@
 using Blazored.LocalStorage;
+using Blazored.Modal;
 using BoxOfficeDemo.Client;
-using BoxOfficeDemo.Client.Services;
 using BoxOfficeDemo.Client.Services.Auth;
 using BoxOfficeDemo.Client.Services.Movies;
 using BoxOfficeDemo.Client.Services.Reviews;
@@ -9,20 +9,20 @@ using BoxOfficeDemo.Client.Services.Watchlist;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7227/api/") }.EnableIntercept(sp));
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://mostafagamal68.bsite.net/api/") }.EnableIntercept(sp));
 //builder.HostEnvironment.BaseAddress
 builder.Services.AddScoped<IMoviesService,MoviesService>();
 builder.Services.AddScoped<IReviewsService,ReviewsService>();
 builder.Services.AddScoped<IWatchListService, WatchListService>();
 builder.Services.AddScoped<IToastService, ToastService>();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredModal();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddHttpClientInterceptor();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
