daf49ffd805afb23853301c5d31a6849bce38133
Mostafa Gamal <mostafa.gamal_98@hotmail.com>
2023-01-22 00:39:25 +0200

    Themes using c# instead js and components inherits from CustomComponentsBase

---
+++ BoxOfficeDemo.Client/Configurations/Theme.cs
@@ -0,0 +1,9 @@
+﻿namespace BoxOfficeDemo.Client.Configurations
+{
+    public class Theme
+    {
+        public static string ThemeName { get; set; } = "";
+        public static string BorderColor { get; set; } = "";
+        public static string BackColor { get; set; } = "";
+    }
+}
