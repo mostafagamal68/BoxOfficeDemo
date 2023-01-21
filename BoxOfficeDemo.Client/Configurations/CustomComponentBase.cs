da070060830079f59e47ede2661ea77a94a74c0d
Mostafa Gamal <mostafa.gamal_98@hotmail.com>
2023-01-22 00:39:49 +0200

    CustomComponentBase Class added with injections

---
+++ BoxOfficeDemo.Client/Configurations/CustomComponentBase.cs
@@ -0,0 +1,28 @@
+﻿using Blazored.LocalStorage;
+using Blazored.Modal.Services;
+using BoxOfficeDemo.Client.Services.Toast;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BoxOfficeDemo.Client.Configurations
+{
+    public class CustomComponentBase : ComponentBase
+    {
+        [Inject]
+        public ILocalStorageService localStorage { get; set; }
+        [Inject]
+        public IJSRuntime JS { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        [Inject]
+        public IModalService ModalService { get; set; }
+        [Inject]
+        public IToastService ToastService { get; set; }
+        [Inject]
+        public IAuthorizationService AuthorizationService { get; set; }
+        //[Inject]
+        //public Theme Theme { get; set; }
+        public bool IsLoading { get; set; }        
+    }
+}
---
+++ BoxOfficeDemo.Client/Configurations/CustomLayoutComponentBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BoxOfficeDemo.Client.Configurations
+{
+    public class CustomLayoutComponentBase : LayoutComponentBase
+    {
+        
+    }
+}
