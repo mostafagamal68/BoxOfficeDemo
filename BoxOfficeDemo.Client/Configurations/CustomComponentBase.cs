4cb6165fc9d3e3ed2ea8e784280f3fb81a636cbc
Mostafa Gamal <mostafa.gamal_98@hotmail.com>
2023-01-22 23:53:51 +0200

    currentsession injection added and fixed navigatetopage function

--- BoxOfficeDemo.Client/Configurations/CustomComponentBase.cs
+++ BoxOfficeDemo.Client/Configurations/CustomComponentBase.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.JSInterop;
+using System;
 
 namespace BoxOfficeDemo.Client.Configurations
 {
@@ -22,6 +23,8 @@ namespace BoxOfficeDemo.Client.Configurations
         public IToastService ToastService { get; set; }
         [Inject]
         public IAuthorizationService AuthorizationService { get; set; }
+        [Inject]
+        public CurrentSession CurrentUser { get; set; }
 
         [CascadingParameter(Name = "DateFormat")]
         public string DateFormat { get; set; }
@@ -36,12 +39,12 @@ namespace BoxOfficeDemo.Client.Configurations
         public Task<AuthenticationState> authenticationStateTask { get; set; }
 
         public bool IsLoading { get; set; }
-        public void NavigateToPage(string pagename,decimal? id = null)
+        public void NavigateToPage(string pagename, decimal? id = null)
         {
             if (id != null)
                 NavigationManager.NavigateTo($"/{pagename}/{id.ToString().Replace('.', 'O')}");
             else
-                NavigationManager.NavigateTo("/"+ pagename);
+                NavigationManager.NavigateTo("/" + pagename);
         }
     }
 }
